666a7bbb6a7bb7108eac5fee76c4e5af88f7a7cf
fabriceRegueme <fabrice.regueme@gmail.com>
2023-11-20 10:15:27 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import colors from 'colors';
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log(`Connected to MongoDB`.bgYellow);
+}).catch((err) => {
+    console.log(err);
+});
+
+
 const app = express();
 const PORT = process.env.PORT || 3000
 
 
+
 app.listen(5000, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`.bgCyan.bold)
 });  
